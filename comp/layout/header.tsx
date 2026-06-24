41cb08c825c0b176ad736a86d4b97384fb4088be
dxdie <supergavrilov9@gmail.com>
2026-06-24 20:05:01 +0500

    add layout structure with header footer, router works

---
+++ comp/layout/header.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <div className="flex justify-between">
+            <Image
+                src="/150x150.png"
+                alt="Icon"
+                width="150"
+                height="150"
+                loading="eager"
+            />
+            <ul className="flex gap-4 mr-4">
+                <li>
+                    <Link
+                        className="cursor-pointer hover:text-violet-400"
+                        href="/search"
+                    >
+                        Search
+                    </Link>
+                </li>
+                <li>
+                    <a
+                        className="cursor-pointer hover:text-violet-400"
+                        href="/library"
+                    >
+                        Library
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className="cursor-pointer hover:text-violet-400"
+                        href="/profile"
+                    >
+                        Profile
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+}
